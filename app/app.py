afbcb8927a0db4420eaa4ad825922535a64d215a
septa97 <jancharles.adona@gmail.com>
2017-02-19 18:42:47 +0800

    Added CORS and modified requirements.txt

--- app/app.py
+++ app/app.py
@@ -1,11 +1,13 @@
 import os
 from flask import Flask
+from flask_cors import CORS, cross_origin
 from app.config import config
 
 def create_app(env):
 	app = Flask(__name__)
+	CORS(app)
 	app.config.from_object(config[env])
-	
+
 	@app.route('/')
 	def index():
 		return 'Hello World!'
