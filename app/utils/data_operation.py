de3025dff10f5504bde66d2dc9fa4f72a8c5185e
septa97 <jancharles.adona@gmail.com>
2017-03-15 20:41:26 +0800

    Added data_operation.py

---
+++ app/utils/data_operation.py
@@ -0,0 +1,14 @@
+import numpy as np
+
+def accuracy_score(actual, predicted):
+	"""
+	Computes the accuracy
+	"""
+	m = actual.size
+	correct = 0
+
+	for i in range(0, m):
+		if (actual[i] == predicted[i]):
+			correct += 1
+
+	return correct / m
