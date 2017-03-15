a65b07c7a79cd391877934e3ac11b76dde551927
septa97 <jancharles.adona@gmail.com>
2017-03-15 20:35:15 +0800

    Added GET routes for /api/v1/coursera

--- app/api/coursera/routes.py
+++ app/api/coursera/routes.py
@@ -1,5 +1,6 @@
 import json
 import requests
+import os
 from flask import Blueprint, jsonify
 
 # This blueprint is a wrapper for calling the Coursera Catalog API
@@ -9,6 +10,79 @@ instructors_URL = 'https://api.coursera.org/api/instructors.v1'
 
 mod = Blueprint('coursera', __name__)
 
+
+@mod.route('/courses', methods=['GET'])
+def get_all_courses():
+	"""
+	Retrieves all the courses and their fields
+	"""
+	fields = ','.join(['id', 'slug', 'courseType', 'name', 'primaryLanguages',
+						'subtitleLanguages', 'partnerLogo', 'instructorIds', 'partnerIds',
+						'photoUrl', 'certificates', 'description', 'startDate', 'workload',
+						'previewLink', 'specializations', 's12nIds', 'domainTypes', 'categories'])
+
+	data = {'elements': []}
+
+	res = requests.get(courses_URL + '?fields=' + fields + '&start=0&limit=100').json()
+	data['elements'].extend(res['elements'])
+
+	while (res['paging'].get('next') != None):
+		res = requests.get(courses_URL + '?fields=' + fields + '&start=' + res['paging']['next'] + '&limit=100').json()
+		data['elements'].extend(res['elements'])
+
+	return jsonify(data)
+
+@mod.route('/instructors', methods=['GET'])
+def get_all_instructors():
+	"""
+	Retrieves all the instructors and their fields
+	"""
+	fields = ','.join(['id', 'photo', 'photo150', 'bio', 'prefixName', 'firstName', 'middleName',
+						'lastName', 'suffixName', 'fullName', 'title', 'department', 'website',
+						'websiteTwitter', 'websiteFacebook', 'websiteLinkedin', 'websiteGplus', 'shortName'])
+
+	data = {'elements': []}
+
+	res = requests.get(instructors_URL + '?fields=' + fields + '&start=0&limit=100').json()
+	data['elements'].extend(res['elements'])
+
+	while (res['paging'].get('next') != None):
+		res = requests.get(instructors_URL + '?fields=' + fields + '&start=' + res['paging']['next'] + '&limit=100').json()
+		data['elements'].extend(res['elements'])
+
+	return jsonify(data)
+
+@mod.route('/partners', methods=['GET'])
+def get_all_partners():
+	"""
+	Retrieves all the partners and their fields
+	"""
+	fields = ','.join(['id', 'name', 'shortName', 'description', 'banner', 'courseIds', 'instructorIds',
+						'primaryColor', 'logo', 'squareLogo', 'rectangularLogo', 'links', 'location'])
+
+	data = {'elements': []}
+
+	res = requests.get(partners_URL + '?fields=' + fields + '&start=0&limit=100').json()
+	data['elements'].extend(res['elements'])
+
+	while (res['paging'].get('next') != None):
+		res = requests.get(partners_URL + '?fields=' + fields + '&start=' + res['paging']['next'] + '&limit=100').json()
+		data['elements'].extend(res['elements'])
+
+	return jsonify(data)
+
 @mod.route('/partners/location', methods=['GET'])
-def get_all_reviews():
-	return jsonify(requests.get(partners_URL + '?fields=location').json())
+def get_all_partners_location():
+	"""
+	Retrieves all the partner locations
+	"""
+	data = {'elements': []}
+
+	res = requests.get(partners_URL + '?fields=location&start=0&limit=100').json()
+	data['elements'].extend(res['elements'])
+
+	while (res['paging'].get('next') != None):
+		res = requests.get(partners_URL + '?fields=location&start=' + res['paging']['next'] + '&limit=100').json()
+		data['elements'].extend(res['elements'])
+
+	return jsonify(data)
