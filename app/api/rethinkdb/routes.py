ccf6f3a8b4e3e656a596a77e0fe9289531985244
septa97 <jancharles.adona@gmail.com>
2017-03-25 16:13:34 +0800

    Added routes for retrieving and saving courses, instructors, and partners. Also added routes for wordcloud visualization

--- app/api/rethinkdb/routes.py
+++ app/api/rethinkdb/routes.py
@@ -1,14 +1,17 @@
 import rethinkdb as r
-from flask import Blueprint, jsonify
-from app.modules.config import config
-from app.lib.rethinkdb import connection
 
-DB_NAME = config['DB_NAME']
+from flask import Blueprint, jsonify
+from nltk.corpus import sentiwordnet as swn
+from app.configuration.config import config
+from app.lib.rethinkdb_connect import connection
+from app.modules.preprocessor import preprocess
+
+
 mod = Blueprint('rethinkdb', __name__)
 
 @mod.route('/reviews', methods=['GET'])
 def get_all_reviews():
-	cursor = r.db(DB_NAME).table('reviews').run(connection)
+	cursor = r.db(config['DB_NAME']).table('reviews').run(connection)
 	reviews = []
 	rows = []
 
@@ -20,3 +23,224 @@ def get_all_reviews():
 		reviews.extend(row[slug])
 
 	return jsonify(reviews)
+
+
+@mod.route('/courses', methods=['GET'])
+def get_all_courses():
+	"""
+	Retrieves all the courses and their fields from the current data stored on RethinkDB server
+	"""
+	data = {'courses': []}
+
+	cursor = r.db(config['DB_NAME']).table('courses').run(connection)
+
+	for document in cursor:
+		data['courses'].append(document)
+
+	return jsonify(data)
+
+
+@mod.route('/instructors', methods=['GET'])
+def get_all_instructors():
+	"""
+	Retrieves all the instructors and their fields from the current data stored on RethinkDB server
+	"""
+	data = {'instructors': []}
+
+	cursor = r.db(config['DB_NAME']).table('instructors').run(connection)
+
+	for document in cursor:
+		data['instructors'].append(document)
+
+	return jsonify(data)
+
+
+@mod.route('/partners', methods=['GET'])
+def get_all_partners():
+	"""
+	Retrieves all the partners and their fields from the current data stored on RethinkDB server
+	"""
+	data = {'partners': []}
+
+	cursor = r.db(config['DB_NAME']).table('partners').run(connection)
+
+	for document in cursor:
+		data['partners'].append(document)
+
+	return jsonify(data)
+
+
+@mod.route('/partners/location', methods=['GET'])
+def get_all_partners_location():
+	"""
+	Retrieves all the partner locations from the RethinkDB server
+	"""
+	data = {'elements': []}
+
+	cursor = r.db(config['DB_NAME']).table('partners').pluck('id', 'location').run(connection)
+
+	for document in cursor:
+		data['elements'].append(document)
+
+	return jsonify(data)
+
+
+@mod.route('/course/<course_id>/reviews/preprocessed-words/overall', methods=['GET'])
+def get_all_course_review_words_overall(course_id):
+	"""
+	Retrieves all the preprocessed words
+	"""
+	data = {'words': []}
+
+	cursor = r.db(config['DB_NAME']).table('reviews').filter({
+			'id': course_id
+		}).run(connection)
+
+	row = []
+	for document in cursor:
+		row.append(document)
+
+	slug = list(filter(lambda k: k != 'id', row[0].keys()))[0]
+	reviews = row[0][slug]
+
+	for review in reviews:
+		tokens = preprocess(review)
+
+		if (tokens == -1):
+			continue
+
+		data['words'].extend(tokens)
+
+	return jsonify(data)
+
+
+@mod.route('/course/<course_id>/reviews/preprocessed-words/positive', methods=['GET'])
+def get_all_course_review_words_positive(course_id):
+	"""
+	Retrieves all the preprocessed positive words
+	"""
+	data = {'words': []}
+
+	cursor = r.db(config['DB_NAME']).table('reviews').filter({
+			'id': course_id
+		}).run(connection)
+
+	row = []
+	for document in cursor:
+		row.append(document)
+
+	slug = list(filter(lambda k: k != 'id', row[0].keys()))[0]
+	reviews = row[0][slug]
+
+	for review in reviews:
+		tokens = preprocess(review)
+
+		if (tokens == -1):
+			continue
+
+		# Check each word if positive
+		words = []
+		for word in tokens:
+			synset_list = list(swn.senti_synsets(word))
+
+			if (len(synset_list) == 0):
+				continue
+
+			score = 0
+			for synset in synset_list:
+				score += (synset.pos_score() - synset.neg_score())
+
+			if (score > 0):
+				words.append(word)
+
+		data['words'].extend(words)
+
+	return jsonify(data)
+
+
+@mod.route('/course/<course_id>/reviews/preprocessed-words/negative', methods=['GET'])
+def get_all_course_review_words_negative(course_id):
+	"""
+	Retrieves all the preprocessed negative words
+	"""
+	data = {'words': []}
+
+	cursor = r.db(config['DB_NAME']).table('reviews').filter({
+			'id': course_id
+		}).run(connection)
+
+	row = []
+	for document in cursor:
+		row.append(document)
+
+	slug = list(filter(lambda k: k != 'id', row[0].keys()))[0]
+	reviews = row[0][slug]
+
+	for review in reviews:
+		tokens = preprocess(review)
+
+		if (tokens == -1):
+			continue
+
+		# Check each word if negative
+		words = []
+		for word in tokens:
+			synset_list = list(swn.senti_synsets(word))
+
+			if (len(synset_list) == 0):
+				continue
+
+			score = 0
+			for synset in synset_list:
+				score += (synset.pos_score() - synset.neg_score())
+
+			if (score < 0):
+				words.append(word)
+
+		data['words'].extend(words)
+
+	return jsonify(data)
+
+
+@mod.route('/course/<course_id>/reviews/preprocessed-words/neutral', methods=['GET'])
+def get_all_course_review_words_neutral(course_id):
+	"""
+	Retrieves all the preprocessed neutral words
+	"""
+	data = {'words': []}
+
+	cursor = r.db(config['DB_NAME']).table('reviews').filter({
+			'id': course_id
+		}).run(connection)
+
+	row = []
+	for document in cursor:
+		row.append(document)
+
+	slug = list(filter(lambda k: k != 'id', row[0].keys()))[0]
+	reviews = row[0][slug]
+
+	for review in reviews:
+		tokens = preprocess(review)
+
+		if (tokens == -1):
+			continue
+
+		# Check each word if neutral
+		words = []
+		for word in tokens:
+			synset_list = list(swn.senti_synsets(word))
+
+			if (len(synset_list) == 0):
+				continue
+
+			objectivity_score = 0
+			for synset in synset_list:
+				objectivity_score += synset.obj_score()
+
+			if (objectivity_score/len(synset_list) >= 0.875):
+				words.append(word)
+
+		data['words'].extend(words)
+
+	return jsonify(data)
