b1db9545b3ee6600eac3c2cd4fa8b19126804988
septa97 <jancharles.adona@gmail.com>
2017-04-02 21:37:38 +0800

    Modified all the models, used scipy.sparse.csr_matrix instead

--- app/models/LR.py
+++ app/models/LR.py
@@ -3,41 +3,46 @@ import os
 import numpy as np
 import rethinkdb as r
 
-from sklearn import linear_model
+from sklearn.linear_model import LogisticRegression
+from sklearn.datasets import load_files
 from sklearn.metrics import confusion_matrix
+from sklearn.feature_extraction.text import CountVectorizer
 
-from app.configuration.config import config
 from app.lib.rethinkdb_connect import connection
 from app.utils.data_manipulation import feature_scale, train_test_split
 from app.utils.data_operation import accuracy_score
-from app.utils.data_plotting import plot_confusion_matrix
+dir_path = os.path.dirname(os.path.realpath(__file__))
 
 
-def main(test_size=0.2):
-	# Load X and y
-	X = np.array(r.db(config['DB_NAME']).table('X').nth(0).run(connection)['X'])
-	y = np.array(r.db(config['DB_NAME']).table('y').nth(0).run(connection)['y'])
+def main(test_size=0.2, min_df=5):
+	"""
+	Perform Logistic Regression on the current data
+	"""
 
-	# Split the data into training and testing set
-	X_train, X_test, y_train, y_test = train_test_split(X, y, test_size=test_size)
+	reviews = load_files(dir_path + '/../data')
+
+	split = int(len(reviews.data) * (1 - test_size))
+	text_train, y_train = reviews.data[:split], reviews.target[:split]
+	text_test, y_test = reviews.data[split:], reviews.target[split:]
+
+	vect = CountVectorizer(min_df=min_df, stop_words="english").fit(text_train)
+	X_train = vect.transform(text_train)
+	X_test = vect.transform(text_test)
 
 	# Logistic Regression (one vs rest strategy)
-	clf = linear_model.LogisticRegression(solver='newton-cg', multi_class='ovr', verbose=True)
+	clf = LogisticRegression(solver='newton-cg', multi_class='ovr', verbose=True)
 	clf.fit(X_train, y_train)
 
-	training_score = clf.score(X_train, y_train)
+	train_score = clf.score(X_train, y_train)
 	test_score = clf.score(X_test, y_test)
 
-	print('(sklearn) Training data accuracy:', training_score)
+	print('(sklearn) Train data accuracy:', train_score)
 	print('(sklearn) Test data accuracy:', test_score)
 
-	return training_score, test_score, X_train.shape[0]
+	cm_train = confusion_matrix(y_train, clf.predict(X_train))
+	cm_test = confusion_matrix(y_test, clf.predict(X_test))
 
-	# Compute and plot the confusion matrix
-	# plt.figure()
-	# cnf_matrix = confusion_matrix(y_test, clf.predict(X_test))
-	# plot_confusion_matrix(cnf_matrix, target_names, title='Confusion matrix, without normalization')
-	# mpld3.show()
+	return train_score, test_score, X_train.shape[0], X_test.shape[0], cm_train, cm_test
 
 
 if __name__ == "__main__":
